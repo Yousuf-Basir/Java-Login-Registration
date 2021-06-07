b7752abe30e60f5f893ef00b23695b3866f8cff9
Gourab <gourabxz@gmail.com>
2021-06-08 02:06:03 +0600

    AppUser fixed

--- src/main/java/com/example/CompleteLogin/AppUser/AppUser.java
+++ src/main/java/com/example/CompleteLogin/AppUser/AppUser.java
@@ -38,8 +38,12 @@ public class AppUser implements UserDetails {
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+
+
+    private Boolean locked = false;
+    private Boolean enabled = true;
+
+
 
     public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
         this.firstName = firstName;
